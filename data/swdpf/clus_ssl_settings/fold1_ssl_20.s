ed78e67c88fced12bd7ff4e980bc97ab3ebc3553
Nicolas Pinto <ningo1797@gmail.com>
2025-07-05 16:33:26 +0200

    Add SSL configuration files for folds 0 to 7

---
+++ data/swdpf/clus_ssl_settings/fold1_ssl_20.s
@@ -0,0 +1,52 @@
+[General]
+RandomSeed = 42
+
+[Data]
+File = C:/Users/Ningo/Desktop/S2R-Wind/data/swdpf/clus_ssl_arffs/fold1_ssl_20.arff
+TestSet = None
+PruneSet = None
+
+[Attributes]
+Target = 20
+Weights = Normalize
+
+[Model]
+MinimalWeight = 2.0
+
+[Tree]
+Heuristic = VarianceReduction
+FTest = 1.0
+ConvertToRules = No
+
+[Ensemble]
+EnsembleMethod = RForest
+Iterations = 30
+
+[SemiSupervised]
+SemiSupervisedMethod = SelfTraining
+StoppingCriteria = NoneAdded
+Iterations = 1
+UnlabeledCriteria = KMostConfident
+K = 5
+ConfidenceMeasure = Variance
+UseWeights = No
+Normalization = NoNormalization
+Aggregation = Average
+OOBErrorCalculation = LabeledOnly
+PercentageLabeled = 20
+
+[Output]
+ShowModels = Default
+TrainErrors = No
+TestErrors = Yes
+AllFoldModels = No
+AllFoldErrors = No
+AllFoldDatasets = No
+UnknownFrequency = No
+BranchFrequency = No
+ShowInfo = Count
+PrintModelAndExamples = No
+WriteModelFile = No
+WritePredictions = Test
+GzipOutput = No
+
