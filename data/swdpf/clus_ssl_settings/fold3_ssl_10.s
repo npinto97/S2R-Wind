d5923fe23f4b0f471d9895960a297ce8c596da41
Nicolas Pinto <ningo1797@gmail.com>
2025-07-06 13:31:42 +0200

    Update files

--- data/swdpf/clus_ssl_settings/fold3_ssl_10.s
+++ data/swdpf/clus_ssl_settings/fold3_ssl_10.s
@@ -2,51 +2,23 @@
 RandomSeed = 42
 
 [Data]
-File = C:/Users/Ningo/Desktop/S2R-Wind/data/swdpf/clus_ssl_arffs/fold3_ssl_10.arff
-TestSet = None
+File = C:/Users/Ningo/Desktop/S2R-Wind/data/swdpf/clus_ssl_arffs\fold3_ssl_10.arff
+TestSet = C:/Users/Ningo/Desktop/S2R-Wind/data/swdpf/clus_ssl_arffs\fold3_test.arff
 PruneSet = None
 
 [Attributes]
+Descriptive = 1-19
 Target = 20
-Weights = Normalize
-
-[Model]
-MinimalWeight = 2.0
 
 [Tree]
 Heuristic = VarianceReduction
-FTest = 1.0
-ConvertToRules = No
 
 [Ensemble]
+SelectRandomSubspaces = SQRT
 EnsembleMethod = RForest
-Iterations = 30
+FeatureRanking = Genie3
+Iterations = 10 
 
 [SemiSupervised]
-SemiSupervisedMethod = SelfTraining
-StoppingCriteria = NoneAdded
-Iterations = 1
-UnlabeledCriteria = KMostConfident
-K = 5
-ConfidenceMeasure = Variance
-UseWeights = No
-Normalization = NoNormalization
-Aggregation = Average
-OOBErrorCalculation = LabeledOnly
-PercentageLabeled = 10
-
-[Output]
-ShowModels = Default
-TrainErrors = No
-TestErrors = Yes
-AllFoldModels = No
-AllFoldErrors = No
-AllFoldDatasets = No
-UnknownFrequency = No
-BranchFrequency = No
-ShowInfo = Count
-PrintModelAndExamples = No
-WriteModelFile = No
-WritePredictions = Test
-GzipOutput = No
+SemiSupervisedMethod = PCT
 
